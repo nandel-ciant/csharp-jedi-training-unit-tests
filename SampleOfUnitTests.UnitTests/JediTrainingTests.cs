78bccdb29204f78d1d2706e8852770cb4ea8a640
Fernando Ferreira de Souza <fferreiradesouza@axon.com>
2024-04-26 16:53:02 -0300

    Initial commit

---
+++ SampleOfUnitTests.UnitTests/JediTrainingTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Moq.Protected;
+
+namespace SampleOfUnitTests.UnitTests;
+
+public class JediTrainingTests
+{
+    private readonly JediTraining _subject;
+    private readonly Mock<JediTraining> _mock;
+    private readonly JediTrainingStub _stub;
+    
+    private readonly Mock<IJediMastersRepository> _jediMastersRepository = new();
+    private readonly Mock<IConfiguration> _configuration = new();
+
+    public JediTrainingTests()
+    {
+        _subject = new JediTraining(_jediMastersRepository.Object, _configuration.Object);
+        _mock = new Mock<JediTraining>(_jediMastersRepository.Object, _configuration.Object)
+        {
+            CallBase = true 
+        };
+        _stub = new JediTrainingStub(_jediMastersRepository.Object, _configuration.Object);
+    }
+
+    [Fact]
+    public async Task TrainAsync_JediIsSith_ShouldThrownAnException()
+    {
+        // Arrange
+        var jedi = new Jedi()
+        {
+            IsSith = true
+        };
+        
+        // act && assert
+        await _subject.Invoking(x => x.TrainAsync(jedi))
+            .Should()
+            .ThrowAsync<InvalidDataException>();
+    }
+
+    [Fact]
+    public async Task TrainAsync_WhenTrainingIsAvailable_ShouldDoTheTraining()
+    {
+        // Arrange
+        var jedi = new Jedi()
+        {
+            IsSith = false
+        };
+        
+        _mock.Protected()
+            .As<IJediTrainingProtectedMethods>()
+            .Setup(x => x.IsJediTrainingAvailable())
+            .Returns(true);
+
+        _mock.Protected()
+            .As<IJediTrainingProtectedMethods>()
+            .Setup(x => x.TrainInternalAsync(jedi))
+            .Returns(Task.CompletedTask);
+        
+        // Act
+        await _mock.Object.TrainAsync(jedi);
+        
+        // Assert
+        _mock.Protected()
+            .As<IJediTrainingProtectedMethods>()
+            .Verify(x => x.TrainInternalAsync(jedi), times: Times.Once());
+    }
+
+    [Theory]
+    [InlineData("Yoda", 5)]
+    [InlineData("Obi-Wan", 3)]
+    [InlineData("Fernando", 2)]
+    public void GetPowerUp_ShouldMatchExpectedResult(string masterName, int expectedResult)
+    {
+        // arrange
+        var master = new Jedi() { Name = masterName };
+        
+        // act
+        var result = _stub.GetPowerUpPublic(master);
+        
+        // assert
+        result.Should().Be(expectedResult);
+    }
+
+    private interface IJediTrainingProtectedMethods
+    {
+        bool IsJediTrainingAvailable();
+        Task TrainInternalAsync(Jedi padawan);
+    }
+
+    private class JediTrainingStub : JediTraining
+    {
+        public JediTrainingStub(IJediMastersRepository jediMastersRepository, IConfiguration configuration) : base(jediMastersRepository, configuration)
+        {
+        }
+
+        public int GetPowerUpPublic(Jedi padawan)
+        {
+            return GetPowerUp(padawan);
+        }
+    }
+}
---
+++ SampleOfUnitTests/JediTraining.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SampleOfUnitTests;
+
+public record Jedi
+{
+    public string Name { get; set; } = string.Empty;
+    public bool IsSith { get; set; }
+    public int Power { get; set; }
+}
+
+public interface IJediMastersRepository
+{
+    Task<Jedi> RandomAsync();
+}
+
+public class JediTraining
+{
+    private readonly IJediMastersRepository _jediMastersRepository;
+    private readonly IConfiguration _configuration;
+
+    public JediTraining(IJediMastersRepository jediMastersRepository, IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(jediMastersRepository);
+        ArgumentNullException.ThrowIfNull(configuration);
+        
+        _jediMastersRepository = jediMastersRepository;
+        _configuration = configuration;
+    }
+
+    public async Task TrainAsync(Jedi padawan)
+    {
+        // The Jedi training rules are:
+        // - No Siths can be trained and should throw exceptions if we try to train one
+        // - If "Jedi training is available" we should "find a master and start the training"
+        // - The power ups after training are as follows
+        //    - "Yoda" increases the power by 5
+        //    - "Obi-Wan" increases the power by 3
+        //    - Other masters increases the power by 2
+        
+        if (padawan.IsSith)
+        {
+            throw new InvalidDataException("No siths can be trained in here");
+        }
+        
+        if (IsJediTrainingAvailable())
+        {
+            await TrainInternalAsync(padawan);
+        }
+    }
+
+    protected virtual bool IsJediTrainingAvailable()
+    {
+        return _configuration.GetValue<bool>("IsJediTrainingAvailable");
+    }
+
+    protected virtual async Task TrainInternalAsync(Jedi padawan)
+    {
+        var master = await _jediMastersRepository.RandomAsync();
+        padawan.Power += GetPowerUp(master);
+    }
+
+    protected virtual int GetPowerUp(Jedi master)
+    {
+        return master.Name switch
+        {
+            "Yoda" => 5,
+            "Obi-Wan" => 3,
+            _ => 2
+        };
+    }
+}
